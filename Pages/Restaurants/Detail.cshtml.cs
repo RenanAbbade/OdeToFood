51eb51bd1ae1e6c68dc046a8113cf75e1a9f06d1
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-04-20 17:20:28 -0300

    Fetching Restaurant by ID, and Handling bad requests

--- Pages/Restaurants/Detail.cshtml.cs
+++ Pages/Restaurants/Detail.cshtml.cs
@@ -5,17 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class DetailModel : PageModel
     {
+        private readonly IRestaurantData restaurantData;
 
         public Restaurant Restaurant { get; set; }
-        public void OnGet(int restaurantId)
+
+        public  DetailModel(IRestaurantData restaurantData)
         {
-            Restaurant = new Restaurant();
-            Restaurant.Id = restaurantId;
+            this.restaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {//Implementando um IActionResult para tratar erros de id's não encontrados.
+            //O ActionResult é para controlar o tipo de retorno desta solicitação invalida
+            Restaurant = restaurantData.GetById(restaurantId);
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
         }
     }
 }
