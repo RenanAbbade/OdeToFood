baddbe645b013870d18e2a5ce294bf4f313ffc62
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-04-20 12:27:33 -0300

    Criação de formulário de Restaurants com Bootstrap

--- Pages/Restaurants/List.cshtml.cs
+++ Pages/Restaurants/List.cshtml.cs
@@ -28,7 +28,7 @@ namespace OdeToFood.Pages.Restaurants
         public void OnGet()
         {//Por meio da interface Iconfiguration, consigo acessar o arquivo appsetings, buscando pelo valor da keyword message, e exibindo na tela.
             Message = configuration["Message"];
-            Restaurants = restaurantData.GetAll();
+            //Restaurants = restaurantData.GetRestaturantsByName();
         }
     }
 }
