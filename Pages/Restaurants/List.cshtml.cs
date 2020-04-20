31574ec89a9c3ab05d029b66962bbf06b8434b32
Renan Henrique da Silva Abbade <36777338+RenanAbbade@users.noreply.github.com>
2020-04-20 15:29:40 -0300

    Creating a Detail Restaurant page

--- Pages/Restaurants/List.cshtml.cs
+++ Pages/Restaurants/List.cshtml.cs
@@ -17,6 +17,12 @@ namespace OdeToFood.Pages.Restaurants
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
+        //Utiliza-se BindProperty para que a variavel seja usado tanto para receber request, quanto para uma response, neste caso.
+        //Informa a estrutura do asp.net core que esta classe esta se preprando para executar uma metodo que processa uma solicitacao http, essa propriedade deve receber informaçãoes de solicitação, dizendo ao asp.net para procurar por algo chamado "searchTerm para preencher isso antes de invocar o onget() 
+        //Por padrão essa propriedade só será acionada durante uma operação HTTP POST
+        //Para controlar esse comportamente utilizamos uma flag () e configuramos para suportar o método OnGet()
+        [BindProperty(SupportsGet =true)]
+        public string SearchTerm{ get; set; }
 
         //Solicitando a implementacao do IRestaurantData
         public ListModel(IConfiguration configuration, IRestaurantData restaurantData)
@@ -25,10 +31,13 @@ namespace OdeToFood.Pages.Restaurants
             this.restaurantData = restaurantData;
         }
 
-        public void OnGet()
+        public void OnGet(string searchTerm)
         {//Por meio da interface Iconfiguration, consigo acessar o arquivo appsetings, buscando pelo valor da keyword message, e exibindo na tela.
+            //HttpContext.Request. -> Podemos usar tanto o http context para pegar a request, quanto pegando o parametro do submit como parametro no metodo, que ficara responsavel por procurar um "searchTerm" no httpContext
+            //string pode ser null, então mesmo se o método não encontrar um parametro do request vindo como "searchTerm" ele devolverá null, e continuara, diferentemente se fosse int, no qual lançaria uma Exception
+            
             Message = configuration["Message"];
-            //Restaurants = restaurantData.GetRestaturantsByName();
+            Restaurants = restaurantData.GetRestaturantsByName(SearchTerm);
         }
     }
 }
